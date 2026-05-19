b5d7980e56733c767209ce1f7cf02b093b54815d
shubhamkafle44 <shubhamkafle44@gmail.com>
2026-05-18 19:21:06 -0500

    Dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
+import Dashboard from './components/Dashboard'
 const App = () => {
   return (
-    <div>
-      <h1>Hello Getting Started</h1>
-    </div>
+    <Dashboard />
   )
 }
 
