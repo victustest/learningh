0976efc0a45995a392b6689bd75dbffe6b4337df
Viral Dabhade <viraldabhade@gmail.com>
2026-05-18 19:39:22 -0500

    Create MathAndElapage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Dashboard from './components/Dashboard'
 import Robotix from './components/Robotix'
+import MathAndEla from './components/MathAndEla'
 const App = () => {
   return (
     <div>
       <Dashboard />
       <Robotix />
+      <MathAndEla />
     </div>
   )
 }
---
+++ src/components/MathAndEla.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MathAndEla = () => {
+  return (
+    <div>MathAndEla</div>
+  )
+}
+
+export default MathAndEla
